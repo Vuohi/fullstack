23042c8c80ebba0c5100b88ea3b45593e1394991
Katri Vuori <ktrvuori@gmail.com>
2020-05-19 15:33:32 +0300

    2.11 done

--- kaksi/puhelinluettelo/src/components/Persons.js
+++ kaksi/puhelinluettelo/src/components/Persons.js
@@ -1,7 +1,8 @@
 import React from 'react'
 
 const Persons = ({persons, filter}) => (
-    persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase())).map(person => <li key={person.name}>{person.name} {person.number}</li>)
+    persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
+      .map(person => <li key={person.name}>{person.name} {person.number}</li>)
   )
   
   export default Persons
